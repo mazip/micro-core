c8da9206fed8ed40619be4efc518e0b3f6fe031e
mazip <314059610@qq.com>
2015-06-03 23:59:59 +0800

    2015-6-3

---
+++ src/main/java/com/microframework/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.microframework.constants;
+
+
+public class Constants {
+    public static final String CURRENT_USER = "user";
+
+    public static final String SERVER_APP_KEY = "645ba616-370a-43a8-a8e0-993e7a590cf0";
+}
