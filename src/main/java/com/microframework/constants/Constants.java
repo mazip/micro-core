7273c4786a63b53aced83a61caf2d822b8cee234
mazip <314059610@qq.com>
2015-06-04 00:16:14 +0800

    modify constats

--- src/main/java/com/microframework/constants/Constants.java
+++ src/main/java/com/microframework/constants/Constants.java
@@ -4,5 +4,6 @@ package com.microframework.constants;
 public class Constants {
     public static final String CURRENT_USER = "user";
 
+    public static final String BRANCH="0.0.1";
     public static final String SERVER_APP_KEY = "645ba616-370a-43a8-a8e0-993e7a590cf0";
 }
